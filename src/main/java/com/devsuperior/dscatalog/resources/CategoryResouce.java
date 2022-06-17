0f34c91a67b569b47238af853ba6c225cacbffa1
1van14 <ivansilvalves@outlook.com>
2022-06-16 21:03:24 -0300

    CategoryResource

--- src/main/java/com/devsuperior/dscatalog/resources/CategoryResouce.java
+++ src/main/java/com/devsuperior/dscatalog/resources/CategoryResouce.java
@@ -18,8 +18,8 @@ public class CategoryResouce {
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
 		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		list.add(new Category(1L, "Book"));
+		list.add(new Category(2L, "Eletronic"));
 		return ResponseEntity.ok().body(list);
 	}
 
